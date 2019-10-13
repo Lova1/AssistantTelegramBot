0d418212732025695669df169d135f374b90a77e
Lova1 <lova2010@ukr.net>
2019-10-13 16:53:22 +0300

    Message replies

--- src/main/java/com/telegrambot/bot/BotApplication.java
+++ src/main/java/com/telegrambot/bot/BotApplication.java
@@ -1,12 +1,30 @@
 package com.telegrambot.bot;
 
+import com.telegrambot.bot.config.TelegramConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @SpringBootApplication
 public class BotApplication {
 
 	public static void main(String[] args) {
+
+		// Initialize Api Context
+		ApiContextInitializer.init();
+
+		// Instantiate Telegram Bots API
+		TelegramBotsApi botsApi = new TelegramBotsApi();
+
+		// Register our bot
+		try {
+			botsApi.registerBot(new TelegramConfig());
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
+
 		SpringApplication.run(BotApplication.class, args);
 	}
 
---
+++ src/main/java/com/telegrambot/bot/config/TelegramConfig.java
@@ -0,0 +1,42 @@
+package com.telegrambot.bot.config;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramConfig extends TelegramLongPollingBot {
+
+    private final static String TOKEN = "979792261:AAHH1SWO3vu5OYjBQ61_12Cv72GlmllcMkU";
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Set variables
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+
+            SendMessage message = new SendMessage() // Create a message object object
+                    .setChatId(chat_id)
+                    .setText(message_text);
+            try {
+                execute(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "JavabooBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return TOKEN;
+    }
+}
